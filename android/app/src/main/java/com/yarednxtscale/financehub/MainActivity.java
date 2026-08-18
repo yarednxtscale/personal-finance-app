9b86291fdbc1518e819f03d4769900a3d555bffa
yarednxtscale <yared.mmekonen@gmail.com>
2026-08-19 02:39:44 +0300

    Add native Finance Hub Android activity

---
+++ android/app/src/main/java/com/yarednxtscale/financehub/MainActivity.java
@@ -0,0 +1,78 @@
+package com.yarednxtscale.financehub;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private static final String FINANCE_HUB_URL = "https://personal-finance-hub-supabase.vercel.app/";
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        setContentView(webView);
+
+        CookieManager.getInstance().setAcceptCookie(true);
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        webView.getSettings().setUserAgentString(webView.getSettings().getUserAgentString() + " FinanceHubAndroid/1.0");
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                view.evaluateJavascript("(function(){var remove=function(){document.querySelectorAll('.pwa-install').forEach(function(e){e.remove();});};remove();new MutationObserver(remove).observe(document.documentElement,{childList:true,subtree:true});})();", null);
+            }
+        });
+
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(android.os.Environment.DIRECTORY_DOWNLOADS, "FinanceHub-export");
+                ((DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE)).enqueue(request);
+            }
+        });
+
+        if (savedInstanceState == null) webView.loadUrl(FINANCE_HUB_URL);
+        else webView.restoreState(savedInstanceState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack(); else super.onBackPressed();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        webView.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+}
